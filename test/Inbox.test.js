5e3314d718dfa3de2b624dc427869a72236db07d
Pablo Rocha <pablorochatmm@gmail.com>
2021-09-21 17:57:55 -0500

    example to learn motcha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,3 +5,31 @@ const Web3 = require("web3");
 
 // exp create a new instance of web 3, the argument is the etherum network we are connecting to
 const web3 = new Web3(ganache.provider());
+
+//exp clase para probar motcha
+class Car {
+	park() {
+		return "stopped";
+	}
+	drive() {
+		return "brum";
+	}
+}
+
+// Motcha
+
+// exp no hay relacion con el string y la clase, solo es referencia
+// e2 esto se ejecuta antes de cada it en el describe
+let car;
+beforeEach(() => {
+	car = new Car();
+});
+// e2 son pruebas, cada it es una prueba
+describe("Car Class", () => {
+	it("It can park", () => {
+		assert.equal(car.park(), "stopped");
+	});
+	it("It can drive", () => {
+		assert.equal(car.drive(), "brum");
+	});
+});
